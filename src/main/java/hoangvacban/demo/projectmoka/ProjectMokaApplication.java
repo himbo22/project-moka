3229a6c358f4fe24ac1cc6dc97914a686055c803
just_flash_kit <mhoanga1@gmail.com>
2024-08-01 22:55:03 +0700

    init project and the first commit

---
+++ src/main/java/hoangvacban/demo/projectmoka/ProjectMokaApplication.java
@@ -0,0 +1,13 @@
+package hoangvacban.demo.projectmoka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMokaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMokaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hoangvacban/demo/projectmoka/ProjectMokaApplicationTests.java
@@ -0,0 +1,13 @@
+package hoangvacban.demo.projectmoka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMokaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
